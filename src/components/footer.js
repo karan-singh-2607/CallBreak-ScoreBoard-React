82ea83eabbce5dbf79dc977c8c9127663392ead1
karan-singh-2607 <karan.2607.github@gmail.com>
2021-07-04 15:56:23 +0530

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ export default class Footer extends React.Component {
         style={{ height: this.props.height, textAlign: "center" }}
         className="border-top border-primary pt-2"
       >
-        &copy; 2021 | KARAN
+        &copy; 2021 | <a href="https://vanilacodes.com/">KARAN</a>
       </footer>
     );
   }
